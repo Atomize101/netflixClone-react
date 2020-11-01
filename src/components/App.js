37ce1514164b59ced2a17801f1282b05bd2c2910
Chris Patrick <atomize101@gmail.com>
2020-11-01 10:08:06 -0500

    Button

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Global, css, jsx } from '@emotion/core'
 import Navbar from './Navbar'
+import Jumbotron from './Jumbotron'
 
 /**
  * @function App
@@ -9,6 +10,7 @@ const App = () => (
   <>
     <Global styles={GlobalCSS} />
     <Navbar />
+    <Jumbotron />
   </>
 )
 
